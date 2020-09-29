ac330caa1bcc351658127e1caa33a270d15bcfbf
grupoamil\ricanascimento <ricardo.paulino@prestadores.uhgbrasil.com.br>
2020-09-29 15:36:41 -0300

    first program describe the anatomy

---
+++ BasicStructure/BasicStructure/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BasicStructure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("----------- My Program CSharp -------------");
+            Console.ReadKey();
+        }
+    }
+}
